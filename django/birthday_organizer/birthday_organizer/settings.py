51bf27ab747653dd1111a5401348a2ed5ca0c721
Georgi Kunchev <gvkunchev@gmail.com>
2023-04-21 14:32:43 +0300

    Attempt static files collection

--- django/birthday_organizer/birthday_organizer/settings.py
+++ django/birthday_organizer/birthday_organizer/settings.py
@@ -154,6 +154,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "portal/static/js",
     BASE_DIR / "portal/static/css",
 ]
+STATIC_ROOT  = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
