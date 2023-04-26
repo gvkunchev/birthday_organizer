b629953fa9c9e92300958f5aff719e8d6cff298b
Georgi Kunchev <gvkunchev@gmail.com>
2023-04-26 13:13:07 +0300

    Add what's new screen in home

--- django/birthday_organizer/birthday_organizer/settings.py
+++ django/birthday_organizer/birthday_organizer/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Birthday Organizer global version
-VERSION = (1, 0, 4)
+VERSION = (1, 0, 5)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
